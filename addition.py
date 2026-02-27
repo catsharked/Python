2c23b7f953f6c0727ac6b784fc9951abc937bc6b
catsharked <165863668+catsharked@users.noreply.github.com>
2026-02-26 17:52:22 -0800

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,46 @@
+print("Enter up to 10 numbers to do addition. Enter 0 to calculate.")
+adding = true
+answer = number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10
+while adding = true
+  number1 = int(input("Starting number: "))
+  number2 = int(input("+ "))
+  number3 = int(input("+ "))
+  if number3 = 0
+    print("Answer:" answer)
+    adding = false
+
+  number4 = int(input("+ "))
+  if number4 = 0
+    print("Answer:" answer)
+    adding = false
+
+  number5 = int(input("+ "))
+  if number5 = 0
+    print("Answer:" answer)
+    adding = false
+
+  number6 = int(input("+ "))
+  if number6 = 0
+    print("Answer:" answer)
+    adding = false
+
+  number7 = int(input("+ "))
+  if number7 = 0
+    print("Answer:" answer)
+    adding = false
+
+  number8 = int(input("+ "))
+  if number8 = 0
+    print("Answer:" answer)
+    adding = false
+
+  number9 = int(input("+ "))
+  if number9 = 0
+    print("Answer:" answer)
+    adding = false
+
+  number10 = int(input("+ "))
+  if number10 = 0
+    print("Answer:" answer)
+    adding = false
+
