0e70643c064a11ccda66c2bbd5532e5d899e72b2
catsharked <165863668+catsharked@users.noreply.github.com>
2026-03-14 09:45:04 -0700

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,13 +1,9 @@
 print("Enter numbers to do addition. Enter 0 for the answer.")
-adding = true
-
+adding = True
 numbers = []
-while adding = true
+answer = 0
+while adding == True:
  number = int(input("+ "))
  numbers.append(number)
- if number = 0
-    for x in numbers:
-      answer += x
-      numbers = []
-      numbers.append(answer)
-    print("Answer:", answer)
+ answer = sum(numbers)
+ print("=", answer)
